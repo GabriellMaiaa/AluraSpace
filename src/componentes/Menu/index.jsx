401fbc69f1782fb98e411268f6d23139a3e9d757
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-04 11:11:33 -0300

    adicionando banner

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import home from '../../assets/icones/home-ativo.png'//Esse ../ serve para voltar duas pastas
+import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
+import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
+import Novas from '../../assets/icones/novas-inativo.png'
+import Surpreenda from '../../assets/icones/surpreenda-me-inativo.png'
+import styles from './Menu.module.scss'
 
 export default function Menu() {
   return (
-    <div>Menu</div>
+    <nav className={styles.menu}>
+        <ul className={styles.menu__lista}>
+            <li className={styles.menu__item}>
+                <img src={home} alt="home" />
+                <a href="">Início</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={maisCurtidas} alt="curtidas" />
+                <a href="">Mais curtidas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={maisVistas} alt="vistas" />
+                <a href="">Mais vistas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={Novas} alt="novas" />
+                <a href="">Novas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={Surpreenda} alt="surpreenda-me" />
+                <a href="">Surpreenda-me</a>
+            </li>
+        </ul>
+    </nav>
   )
 }
--- src/paginas/PaginaInicial/index.js
+++ src/paginas/PaginaInicial/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Cabecalho from '../../componentes/Cabecalho'
 import Menu from '../../componentes/Menu'
+import Banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
 const index = () => {
   return (
     <>{/**Ao invés de por uma Div, coloca-se o fragment, que não tem nenhuma função semântica   */}
     <Cabecalho/>
-    <Menu/>
+    <main>
+      <section  className={styles.principal}>
+      <Menu/>
+        <div className={styles.principal__imagem}>
+          <h1>A galeria mais completa da Galáxia</h1>
+          <img src={Banner} alt="img da terra vista do " />
+        </div>
+      </section>
+    </main>
     </>
   )
 }
